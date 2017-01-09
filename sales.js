cb4acb6179a05e35e67f09ec1aa1cf958dc12ecc
‘abswhite1git config --global user.email ‘abigailwhite213@gmail.com <abigailwhite213@gmail.comgit config --global user.name abswhite1>
2017-01-09 15:57:56 -0800

    array list functioning

--- sales.js
+++ sales.js
@@ -12,25 +12,37 @@ console.log(customers());
 locationPike.randCustomer = customers();
 console.log(locationPike);
 ////
-*/
+getRandomArtbitrary(min,max)
+  return Math.floor(Math.random() * (this.max-this.min) + this.min);
 
+var locations = ['1st and Pike', 'SeaTac Airport'];
+var minCustomers = [23, 3];
+var maxCustomers = [65, 24];
+var avgCookies = [6.3, 1.2];
+
+function amtCookies() {
+  locationPike.push(Math.floor(Math.random() * (maxCustomers[0] - minCustomers[0] + 1)) + minCustomers[0]) * avgCookies[0];}
+*/
+/////////////////////////
 var locationPike = ['1st and Pike', 23, 65, 6.3];
 console.log(locationPike);
 
 function amtCookies() {
   locationPike.push(Math.floor(Math.random() * (locationPike[2] - locationPike[1] + 1)) + locationPike[1]) * locationPike[3];
 }
+console.log(amtCookies());
 console.log(locationPike);
-/*
+
 var times = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm'];
 
-var locations = document.getElementsById('locationPike');
+var locations = document.getElementById('locationPike');
 
 for (var index = 0; index < times.length; index++) {
   console.log(times[index]);
   var listElement = document.createElement('li');
   listElement.setAttribute = ('class', 'times');
-  listElement.textConent = times[index] + ':';
+  listElement.textContent = times[index] + ': ' +
+  locationPike.push(Math.floor(Math.random() * (locationPike[2] - locationPike[1] + 1)) + locationPike[1]) * locationPike[3];
   locations.appendChild(listElement);
 }
 /*
@@ -47,13 +59,14 @@ function amtCookies() {
 locationPike.push(amtCookies());
 console.log(locationPike);
 console.log(amtCookies());
-
+*/
+/*
 function amtCookies() {
   locationPike.push(Math.floor(Math.random() * (locationPike[2] - locationPike[1] + 1)) + locationPike[1]) * locationPike[3];
 }
 console.log(amtCookies());
 console.log(locationPike);
-*/
+
 /*
 function amtCookies() {
   amtCustomers
