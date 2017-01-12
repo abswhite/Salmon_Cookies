3c5bf34c98c716a03d36b4d44bf5d4cb517cb5b3
‘abswhite1git config --global user.email ‘abigailwhite213@gmail.com <abigailwhite213@gmail.comgit config --global user.name abswhite1>
2017-01-12 15:22:02 -0800

    times added to table

--- sales.js
+++ sales.js
@@ -48,6 +48,21 @@ Location.prototype.totals = function() {
 //   }
 // };
 
+function renderTimes() {
+  var cookieTable = document.getElementById('cookie-table');
+
+  var timesRow = document.createElement('tr');
+
+  for (var i = 0; i < times.length; i++) {
+    var timesElement = document.createElement('th');
+    console.log(times[i]);
+    timesElement.textContent = times[i];
+    timesRow.appendChild(timesElement);
+  };
+  cookieTable.appendChild(timesRow);
+  console.log('im in renderTimes');
+};
+
 Location.prototype.render = function() {
   var cookieTable = document.getElementById('cookie-table');
 
@@ -70,6 +85,8 @@ Location.prototype.render = function() {
 
   cookieTable.appendChild(cookieRow);
 };
+
+renderTimes();
 
 var locationPike = new Location('1st and Pike', 23, 65, 6.3);
 console.log(locationPike);
@@ -101,6 +118,16 @@ locationAlki.cookies();
 locationAlki.totals();
 locationAlki.render();
 
+//New Location
+var formEl = document.getElementById('cookie-form');
+
+formEl.addEventListener('submit', function(event) {
+  event.preventDefault();
+  event.stopPropagation();
+
+  
+}
+
 // //New Location
 // var formEl = document.getElementById('cookie-form');
 //
@@ -110,133 +137,131 @@ locationAlki.render();
 //
 
 ///NEW LOCATION
-
-var formEl = document.getElementById('cookie-form');
-
-formEl.addEventListener('submit', function(event){
-  event.preventDefault();
-  event.stopPropagation();
-  nameEl.name = renderName(event.target.name.value);
-  this.min = parseInt(renderMin(event.target.min.value));
-  this.max = parseInt(renderMax(event.target.max.value));
-  this.avgCookies = parseInt(renderAvgCookies(event.target.avgCookies.value));
-  console.log(this.min);
-  console.log(nameEl.name);
-
-  // renderName(event.target.name.value);
-  // renderMin(event.target.min.value);
-  // renderMax(event.target.max.value);
-  // renderAvgCookies(event.target.avgCookies.value);
-}, false);
-
-function renderName(name) {
-  var sectionEl = document.getElementById('cookie-section');
-
-  var nameEl = document.createElement('td');
-  nameEl.setAttribute('class', 'entered-value');
-  nameEl.textContent = name;
-  console.log(name);
-  newLoc.push(name);
-
-  sectionEl.appendChild(nameEl);
-};
-function renderMin(min) {
-  var sectionEl = document.getElementById('cookie-section');
-
-  var minEl = document.createElement('td');
-  minEl.setAttribute('class', 'entered-value');
-  minEl.textContent = min;
-  console.log(min);
-  sectionEl.appendChild(minEl);
-};
-function renderMax(max) {
-  var sectionEl = document.getElementById('cookie-section');
-
-  var maxEl = document.createElement('td');
-  maxEl.setAttribute('class', 'entered-value');
-  maxEl.textContent = max;
-  console.log(max);
-
-  sectionEl.appendChild(maxEl);
-};
-function renderAvgCookies(avgCookies) {
-  var sectionEl = document.getElementById('cookie-section');
-
-  var avgCookiesEl = document.createElement('td');
-  avgCookiesEl.setAttribute('class', 'entered-value');
-  avgCookiesEl.textContent = avgCookies;
-  console.log(avgCookies);
-
-  sectionEl.appendChild(avgCookiesEl);
-};
-var newLoc = [formEl.name, formEl.min, formEl.max, formEl.avgCookies];
-console.log(formEl.name);
-console.log(newLoc);
-
-var locationNew = new Location(newLoc[0], parseInt(newLoc[1]), parseInt(newLoc[2]), parseInt(newLoc[3]),[]);
-console.log(locationNew);
-
-var myList = document.getElementById('locationNew');
-
-for (var index = 0; index < times.length; index++) {
-  console.log(times[index]);
-  var listElement = document.createElement('li');
-  listElement.setAttribute ('class', 'times');
-  listElement.textContent = times[index] + ': ' +
-  locationNew.cookies() + ' cookies';
-  //myList.appendChild(listElement);
-}
-
-var s = 0;
-for(var i = 0; i < locationNew.arrayCookies.length; i++) {
-  s += locationNew.arrayCookies[i];
-};
-totals.push(s);
-console.log(totals);
-
-var locationsTotal = document.getElementById('locationNew');
-var listElement = document.createElement('li');
-listElement.setAttribute ('class', 'cookieTotal');
-listElement.textContent = 'Total: ' + s + ' cookies';
-//locationsTotal.appendChild(listElement);
-////////////
-
-//times , arrayCookies
-locationPike.arrayCookies.unshift('1st and Pike');
-locationPike.arrayCookies.push(totals[0]);
-locationCenter.arrayCookies.unshift('Seattle Center');
-locationCenter.arrayCookies.push(totals[2]);
-locationAirport.arrayCookies.unshift('SeaTac Airport');
-locationAirport.arrayCookies.push(totals[1]);
-locationCapitolHill.arrayCookies.unshift('Capitol Hill');
-locationCapitolHill.arrayCookies.push(totals[3]);
-locationAlki.arrayCookies.unshift('Alki');
-locationAlki.arrayCookies.push(totals[4]);
-times.unshift('');
-times.push('Total');
-locationNew.arrayCookies.unshift('New Location');
-locationNew.arrayCookies.push(totals[5]);
-
-var totalArray = [locationPike.arrayCookies, locationAirport.arrayCookies, locationCenter.arrayCookies, locationCapitolHill.arrayCookies, locationAlki.arrayCookies, locationNew.arrayCookies];
-console.log(totalArray);
-
-var tableEl = document.getElementById('cookie-table');
-
-
-
-for(var i = 0; i < totalArray.length; i++) {
-  var rowData = totalArray[i];
-
-  var rowEl = document.createElement('tr');
-
-  for (var j = 0; j < rowData.length; j++) {
-    var content = rowData[j];
-
-    var dataEl = document.createElement('td');
-    dataEl.textContent = content;
-    console.log('rowel',rowEl);
-    rowEl.appendChild(dataEl);
-  }
-
-  tableEl.appendChild(rowEl);
-};
+//
+// var formEl = document.getElementById('cookie-form');
+//
+// formEl.addEventListener('submit', function(event){
+//   event.preventDefault();
+//   event.stopPropagation();
+//   nameEl.name = renderName(event.target.name.value);
+//   this.min = parseInt(renderMin(event.target.min.value));
+//   this.max = parseInt(renderMax(event.target.max.value));
+//   this.avgCookies = parseInt(renderAvgCookies(event.target.avgCookies.value));
+//   console.log(this.min);
+//   console.log(nameEl.name);
+//
+//   // renderName(event.target.name.value);
+//   // renderMin(event.target.min.value);
+//   // renderMax(event.target.max.value);
+//   // renderAvgCookies(event.target.avgCookies.value);
+// }, false);
+//
+// function renderName(name) {
+//   var sectionEl = document.getElementById('cookie-section');
+//
+//   var nameEl = document.createElement('td');
+//   nameEl.setAttribute('class', 'entered-value');
+//   nameEl.textContent = name;
+//   console.log(name);
+//   newLoc.push(name);
+//
+//   sectionEl.appendChild(nameEl);
+// };
+// function renderMin(min) {
+//   var sectionEl = document.getElementById('cookie-section');
+//
+//   var minEl = document.createElement('td');
+//   minEl.setAttribute('class', 'entered-value');
+//   minEl.textContent = min;
+//   console.log(min);
+//   sectionEl.appendChild(minEl);
+// };
+// function renderMax(max) {
+//   var sectionEl = document.getElementById('cookie-section');
+//
+//   var maxEl = document.createElement('td');
+//   maxEl.setAttribute('class', 'entered-value');
+//   maxEl.textContent = max;
+//   console.log(max);
+//
+//   sectionEl.appendChild(maxEl);
+// };
+// function renderAvgCookies(avgCookies) {
+//   var sectionEl = document.getElementById('cookie-section');
+//
+//   var avgCookiesEl = document.createElement('td');
+//   avgCookiesEl.setAttribute('class', 'entered-value');
+//   avgCookiesEl.textContent = avgCookies;
+//   console.log(avgCookies);
+//
+//   sectionEl.appendChild(avgCookiesEl);
+// };
+// var newLoc = [formEl.name, formEl.min, formEl.max, formEl.avgCookies];
+// console.log(formEl.name);
+// console.log(newLoc);
+//
+// var locationNew = new Location(newLoc[0], parseInt(newLoc[1]), parseInt(newLoc[2]), parseInt(newLoc[3]),[]);
+// console.log(locationNew);
+//
+// var myList = document.getElementById('locationNew');
+//
+// for (var index = 0; index < times.length; index++) {
+//   console.log(times[index]);
+//   var listElement = document.createElement('li');
+//   listElement.setAttribute ('class', 'times');
+//   listElement.textContent = times[index] + ': ' +
+//   locationNew.cookies() + ' cookies';
+//   //myList.appendChild(listElement);
+// }
+//
+// var s = 0;
+// for(var i = 0; i < locationNew.arrayCookies.length; i++) {
+//   s += locationNew.arrayCookies[i];
+// };
+// totals.push(s);
+// console.log(totals);
+//
+// var locationsTotal = document.getElementById('locationNew');
+// var listElement = document.createElement('li');
+// listElement.setAttribute ('class', 'cookieTotal');
+// listElement.textContent = 'Total: ' + s + ' cookies';
+// //locationsTotal.appendChild(listElement);
+// ////////////
+//
+// //times , arrayCookies
+// locationPike.arrayCookies.unshift('1st and Pike');
+// locationPike.arrayCookies.push(totals[0]);
+// locationCenter.arrayCookies.unshift('Seattle Center');
+// locationCenter.arrayCookies.push(totals[2]);
+// locationAirport.arrayCookies.unshift('SeaTac Airport');
+// locationAirport.arrayCookies.push(totals[1]);
+// locationCapitolHill.arrayCookies.unshift('Capitol Hill');
+// locationCapitolHill.arrayCookies.push(totals[3]);
+// locationAlki.arrayCookies.unshift('Alki');
+// locationAlki.arrayCookies.push(totals[4]);
+// times.unshift('');
+// times.push('Total');
+// locationNew.arrayCookies.unshift('New Location');
+// locationNew.arrayCookies.push(totals[5]);
+//
+// var totalArray = [locationPike.arrayCookies, locationAirport.arrayCookies, locationCenter.arrayCookies, locationCapitolHill.arrayCookies, locationAlki.arrayCookies, locationNew.arrayCookies];
+// console.log(totalArray);
+//
+// var tableEl = document.getElementById('cookie-table');
+//
+// for(var i = 0; i < totalArray.length; i++) {
+//   var rowData = totalArray[i];
+//
+//   var rowEl = document.createElement('tr');
+//
+//   for (var j = 0; j < rowData.length; j++) {
+//     var content = rowData[j];
+//
+//     var dataEl = document.createElement('td');
+//     dataEl.textContent = content;
+//     console.log('rowel',rowEl);
+//     rowEl.appendChild(dataEl);
+//   }
+//
+//   tableEl.appendChild(rowEl);
+// };
